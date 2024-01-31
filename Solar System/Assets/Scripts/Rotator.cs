23c147f36f038bdd1ce14a3e107d190312e34149
chavezMac <chavom68@gmail.com>
2024-01-30 22:24:18 -0800

    Updated Solar System orbits

--- Solar System/Assets/Scripts/Rotator.cs
+++ Solar System/Assets/Scripts/Rotator.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour {
     public float yawDegreePerSecond = 45f;
+    public float eachPlanetSpeed;
+
+    Transform earthTransform;
     // Start is called before the first frame update
     void Start() {
-        
+       
     }
 
     // Update is called once per frame
     void Update() {
         Transform myTransform = GetComponent<Transform>();
-        myTransform.Rotate(new Vector3(0f, yawDegreePerSecond * Time.deltaTime, 0f), Space.World);
+        myTransform.Rotate(new Vector3(0f, yawDegreePerSecond * Time.deltaTime * eachPlanetSpeed, 0f), Space.World);
     }
 }
